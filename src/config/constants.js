38e29ade3b654f2046d5e3b1453bedc6bc63cb28
Himanshu <sarkarivle@email.com>
2026-06-30 15:31:54 +0530

    Upgrade to Ultra Secure Backend with JWT and RBAC

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,4 +1,4 @@
 module.exports = {
     DEFAULT_RUNPOD_URL: "http://127.0.0.1:11434/api/chat", // Agar Ollama usi VPS par hai toh 127.0.0.1 use karein
-    AI_MODEL_NAME: "llama3.3:70b" // Model name check karein (Ollama list me hona chahiye)
+    AI_MODEL_NAME: "Lora_v1:latest" // Model name check karein (Ollama list me hona chahiye)
 };
