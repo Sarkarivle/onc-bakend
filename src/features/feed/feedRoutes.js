88cf9e1558810be5e2fd950f56e713e7537dd49f
Himanshu <sarkarivle@email.com>
2026-07-14 14:06:58 +0530

    UBAC

--- src/features/feed/feedRoutes.js
+++ src/features/feed/feedRoutes.js
@@ -4,24 +4,11 @@ const authMiddleware = require('../../middlewares/authMiddleware');
 
 const router = express.Router();
 
-const handle = (fnName) => (req, res, next) => {
-    try {
-        const handler = feedController && feedController[fnName];
-        if (typeof handler === 'function') {
-            return handler(req, res, next);
-        }
-
-        throw new Error(`Feed controller method ${fnName} not found`);
-    } catch (err) {
-        return res.status(500).json({ status: 'error', message: err.message });
-    }
-};
-
 router.use(authMiddleware.protect);
 
-router.get('/', handle('getFeed'));
-router.post('/', handle('createPost'));
-router.patch('/:id/like', handle('likePost'));
-router.post('/:id/comment', handle('addComment'));
+router.get('/', feedController.getFeed);
+router.post('/', feedController.createPost);
+router.patch('/:id/like', feedController.likePost);
+router.post('/:id/comment', feedController.addComment);
 
 module.exports = router;
